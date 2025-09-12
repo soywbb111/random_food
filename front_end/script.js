44357dcb4c801fd04551abed2c02b52a3463e32f
soywbb111 <soywbb111@gmail.com>
2025-09-12 23:59:38 +0700

    navbar

--- front_end/script.js
+++ front_end/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("navbar.html")
+  fetch("../navbar.html")
     .then(response => response.text())
     .then(data => {
       document.getElementById("navbar-container").innerHTML = data;
