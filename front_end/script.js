1452b7ac9b07f40b2f1f4e88db8d2aa8d258a31c
soywbb111 <soywbb111@gmail.com>
2025-09-13 23:17:03 +0700

    navbar

--- front_end/script.js
+++ front_end/script.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("../navbar.html")
     .then(response => response.text())
     .then(data => {
-      document.getElementById("navbar-container").innerHTML = data;
+      document.getElementById("navbar").innerHTML = data;
+    })
+    .catch(error => {
+      console.error("Không load được navbar:", error);
     });
 });
